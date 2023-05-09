9b4640f62ff11c1e9c632c253b3fa8d6840d4a5a
BND86 <solodko24@gmail.com>
2023-05-09 19:22:40 +0300

    filling improvements
    Сообщение о некорректном вводе, отсутствие фото

--- tg-bot/handlers/fill_form.py
+++ tg-bot/handlers/fill_form.py
@@ -14,7 +14,7 @@ class Form(StatesGroup):
     patronymic = State() # Состояние для запроса отчества
     email = State() # Состояние для запроса почты
     description = State() # Состояние для запроса описания
-    photo = State()
+    result = State()
 
 # Проверка валидности почты
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
@@ -51,58 +51,63 @@ async def cancel_handler(callback: CallbackQuery, state: FSMContext):
 async def insert_form_phone_1(message: types.Message, state: FSMContext):
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(KeyboardButton(text="Отправить номер", request_contact=True))
-    await message.answer(text='Отправьте мне свой номер', reply_markup=kb)
+    await message.answer(text='Начинаем заполнение заявки на поиск потерянной вещи', reply_markup = kb)
+    await message.answer(text='Отправьте мне свой номер', reply_markup = kb_cancel)
     await message.delete()
     await state.set_state(Form.phone)
 
 async def insert_form_phone_2(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['phone'] = message.contact['phone_number']
-    # Удаляем временную кнопку
     await message.answer('Номер принят', reply_markup=ReplyKeyboardRemove())
     await message.answer('Введите ваше имя', reply_markup = kb_cancel)
-    # Переходим в состояние "name"
     await Form.name.set()
 
 async def process_name(message: types.Message, state: FSMContext):
-    # Сохраняем имя в контексте
-    async with state.proxy() as data:
-        data['name'] = message.text
-    # Запрашиваем фамилию
-    await message.answer("Введите вашу фамилию", reply_markup = kb_cancel)
-    # Переходим в состояние "surname"
-    await Form.surname.set()
+    if ValidFio(message.text) == True:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await message.answer("Введите вашу фамилию", reply_markup = kb_cancel)
+        await Form.surname.set()
+    else:
+        await message.answer("Вы ввели некоректное имя")
 
 async def process_surname(message: types.Message, state: FSMContext):
-    #Сохраняем фамилию в контексте
-    async with state.proxy() as data:
-        data['surname'] = message.text
-    # Запрашиваем отчество
-    await message.answer("Введите ваше отчество", reply_markup = kb_cancel)
-    # Переходим в состояние "patronymic"
-    await Form.patronymic.set()
+    if ValidFio(message.text) == True:
+        async with state.proxy() as data:
+            data['surname'] = message.text
+        await message.answer("Введите ваше отчество", reply_markup = kb_cancel)
+        await Form.patronymic.set()
+    else:
+        await message.answer("Вы ввели некоректную фамилию")
 
 async def process_patronymic(message: types.Message, state: FSMContext):
-    # Сохраняем отчество в контексте
-    async with state.proxy() as data:
-        data['patronymic'] = message.text
-    # Завершаем состояние
-    await Form.email.set()
-    await message.answer('Введите ваш адрес электронной почты', reply_markup = kb_cancel)
+    if ValidFio(message.text) == True:
+        async with state.proxy() as data:
+            data['patronymic'] = message.text
+        await message.answer('Введите ваш адрес электронной почты', reply_markup = kb_cancel)
+        await Form.email.set()
+    else:
+        await message.answer("Вы ввели некоректное отчество")
 
 async def insert_form_email(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['email'] = message.text
-    await Form.description.set()
-    await message.answer('Добавьте краткое описание утерянной вещи', reply_markup = kb_cancel)
+    if isValid(message.text) == True:
+        async with state.proxy() as data:
+            data['email'] = message.text
+        await Form.description.set()
+        await message.answer('Добавьте краткое описание утерянной вещи', reply_markup = kb_cancel)
+    else:
+        await message.answer("Вы ввели некоректный адрес электронной почты")
 
 async def insert_form_description(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['description'] = message.text
-    await message.answer('Вы также можете отправить фото', reply_markup = kb_cancel)
-    await Form.photo.set()
+    no_photo = ReplyKeyboardMarkup(resize_keyboard=True)
+    no_photo.add(KeyboardButton(text="Без фотографии"))
+    await message.answer('Вы также можете отправить фотографию', reply_markup = no_photo)
+    await Form.result.set()
 
-async def process_photo(message: types.Message, state: FSMContext):
+async def result(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         await message.answer('Ваша заявка сформирована.\n''ПОЖАЛУЙСТА, ПРОВЕРЬТЕ ПРАВИЛЬНОСТЬ ДАННЫХ!\n'
                              'При необходимости выберите данные для изменения.\n'
@@ -110,21 +115,22 @@ async def process_photo(message: types.Message, state: FSMContext):
                              f"ФИО: {data['surname']} {data['name']} {data['patronymic']}\n\n"
                              f"Электронная почта: {data['email']}\n\n"
                              f"Телефон: {data['phone']}\n\n"
-                             f"Утерянная вещь:\n{data['description']}")
+                             f"Утерянная вещь:\n{data['description']}", reply_markup=ReplyKeyboardRemove())
     user_data = await state.get_data()
     print(user_data)
-    await message.photo[-1].download(destination_dir="tgbot")
-    await message.answer_photo(message.photo[-1].file_id)
+    if message.photo:
+        await message.photo[-1].download(destination_dir="tgbot")
+        await message.answer_photo(message.photo[-1].file_id)
     await state.finish()
 
 
 def register_handlers_fill_form(dp: Dispatcher):
     dp.register_message_handler(insert_form_phone_1, commands=['form'])
     dp.register_message_handler(insert_form_phone_2, filters.IsSenderContact(True), content_types='contact', state = Form.phone)
-    dp.register_message_handler(process_name, lambda msg: ValidFio(msg.text) == True, state=Form.name)
-    dp.register_message_handler(process_surname, lambda msg: ValidFio(msg.text) == True, state=Form.surname)
-    dp.register_message_handler(process_patronymic, lambda msg: ValidFio(msg.text) == True, state=Form.patronymic)
-    dp.register_message_handler(insert_form_email, lambda msg: isValid(msg.text) == True, state=Form.email)
+    dp.register_message_handler(process_name, state=Form.name)
+    dp.register_message_handler(process_surname, state=Form.surname)
+    dp.register_message_handler(process_patronymic, state=Form.patronymic)
+    dp.register_message_handler(insert_form_email, state=Form.email)
     dp.register_message_handler(insert_form_description, state=Form.description)
-    dp.register_message_handler(process_photo, content_types=[types.ContentType.PHOTO], state=Form.photo)
+    dp.register_message_handler(result, content_types=['photo', 'text'], state=Form.result)
     dp.register_callback_query_handler(cancel_handler, state='*')
