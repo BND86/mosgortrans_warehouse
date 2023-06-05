5471b8a66ada8317779075f6e5f1ec2c5e90f2dc
BND86 <solodko24@gmail.com>
2023-06-05 21:13:03 +0300

    Update fill_form.py

--- tg-bot/handlers/fill_form.py
+++ tg-bot/handlers/fill_form.py
@@ -5,7 +5,9 @@ from aiogram import types, Dispatcher
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, CallbackQuery
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from tgbot.create_bot import bot
-from pg_connect import connection, cursor, write_photo
+from pg_connect import connection, write_photo
+import psycopg2
+from datetime import datetime, date
 
 
 class Form(StatesGroup):
@@ -35,8 +37,20 @@ def ValidFio(fio):
     return False
 
 regexdate = re.compile(r'(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d')
-def Valid_date(date):
-    if re.fullmatch(regexdate, date):
+curr_date = date.today()
+def Valid_date(info):
+    try:
+        info_date = datetime.strptime(info, '%d-%m-%Y').date()
+    except ValueError:
+        try:
+            info_date = datetime.strptime(info, '%d/%m/%Y').date()
+        except ValueError:
+            try:
+                info_date = datetime.strptime(info, '%d.%m.%Y').date()
+            except ValueError:
+                info_date = datetime.strptime(info, '%d %m %Y').date()
+
+    if re.fullmatch(regexdate, info) and info_date <= curr_date:
         return True
     return False
 
@@ -44,7 +58,7 @@ photo_path = ''
 photo_flag = False
 
 kb_cancel = InlineKeyboardMarkup()
-cancel_button = InlineKeyboardButton(text='Отмена', callback_data='cancel')
+cancel_button = InlineKeyboardButton(text='Отмена❌', callback_data='cancel')
 kb_cancel.add(cancel_button)
 
 async def cancel_handler(callback: CallbackQuery, state: FSMContext):
@@ -57,7 +71,7 @@ async def cancel_handler(callback: CallbackQuery, state: FSMContext):
 
 # Начинаем заполнение формы
 async def insert_form_phone_1(message: types.Message, state: FSMContext):
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb = ReplyKeyboardMarkup()
     kb.add(KeyboardButton(text="Отправить номер", request_contact=True))
     await message.answer(text='Начинаем заполнение заявки на поиск потерянной вещи', reply_markup = kb)
     await message.answer(text='Отправьте мне свой номер', reply_markup = kb_cancel)
@@ -112,7 +126,7 @@ async def insert_form_date(message: types.Message, state: FSMContext):
         async with state.proxy() as data:
             data['date'] = message.text
         await Form.bus.set()
-        await message.answer('Введите номер марштрута', reply_markup = kb_cancel)
+        await message.answer('Введите номер автобуса, в котором была забыта вещь', reply_markup = kb_cancel)
     else:
         await message.answer("Вы ввели некорекную дату")
 
@@ -131,7 +145,7 @@ async def insert_form_short_desc(message: types.Message, state: FSMContext):
 async def insert_form_description(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['description'] = message.text
-    no_photo = ReplyKeyboardMarkup(resize_keyboard=True)
+    no_photo = ReplyKeyboardMarkup()
     no_photo.add(KeyboardButton(text="Без фотографии"))
     await message.answer('Вы также можете отправить фотографию', reply_markup = no_photo)
     await Form.result.set()
@@ -139,47 +153,62 @@ async def insert_form_description(message: types.Message, state: FSMContext):
 async def result(message: types.Message, state: FSMContext):
     global photo_path, photo_flag
     async with state.proxy() as data:
-        await message.answer('Ваша заявка сформирована.\n''ПОЖАЛУЙСТА, ПРОВЕРЬТЕ ПРАВИЛЬНОСТЬ ДАННЫХ!\n'
-                             'При необходимости выберите данные для изменения.\n'
-                             'Если данные верны - нажмите кнопку "Отправить заявку"\n\n'
-                             f"ФИО: {data['surname']} {data['name']} {data['patronymic']}\n\n"
-                             f"Электронная почта: {data['email']}\n\n"
-                             f"Телефон: {data['phone']}\n\n"
-                             f"Дата утери: {data['date']}\n\n"
-                             f"Номер маршрута: {data['bus']}\n\n"
-                             f"Утерянная вещь:\n{data['lost_thing']}\n{data['description']}", reply_markup=ReplyKeyboardRemove())
+        await message.answer('Ваша заявка сформирована.\n'
+                            f"ФИО: {data['surname']} {data['name']} {data['patronymic']}\n\n"
+                            f"Электронная почта: {data['email']}\n\n"
+                            f"Телефон: {data['phone']}\n\n"
+                            f"Дата утери: {data['date']}\n\n"
+                            f"Номер автобуса: {data['bus']}\n\n"
+                            f"Утерянная вещь:\n{data['lost_thing']}\n{data['description']}\n\n"
+                            'ПОЖАЛУЙСТА, ПРОВЕРЬТЕ ПРАВИЛЬНОСТЬ ДАННЫХ!\n'
+                            'При необходимости заполните форму повторно.\n', reply_markup=ReplyKeyboardRemove())
     if message.photo:
         await message.photo[-1].download(destination_dir="tgbot")
         await message.answer_photo(message.photo[-1].file_id)
         file_info = await bot.get_file(message.photo[-1].file_id)
         print(file_info)
-        photo_path = 'C:/Programming/Poteryashki/mosgortrans_warehouse/tg-bot/tgbot/' + file_info.file_path
+        photo_path = 'C:/Programming/Warehouse/mosgortrans_warehouse/tg-bot/tgbot/' + file_info.file_path
         photo_flag = True
-    await message.answer('подтвердите (ОК)')
+    send_form = InlineKeyboardMarkup()
+    send_form.add(InlineKeyboardButton(text="Отправить заявку", callback_data="Отправить заявку"))
+    send_form.add(InlineKeyboardButton(text="Удалить заявку", callback_data='cancel'))
+    await message.answer('Если данные верны - нажмите кнопку "Отправить заявку"', reply_markup=send_form)
     await Form.send_to_db.set()
 
-async def data_db(message: types.Message, state: FSMContext):
+async def data_db(callback: CallbackQuery, state: FSMContext):
     global photo_path, photo_flag
     user_data = await state.get_data()
     print(user_data)
     await state.finish()
-    cursor.execute(f"""INSERT INTO requests(item_description, comment_, date_time_of_loss, bus_route, email, phone, name_, fam, surname)
-	VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);""",
-    [user_data['lost_thing'],
-    user_data['description'],
-    user_data['date'],
-    user_data['bus'],
-    user_data['email'],
-    user_data['phone'],
-    user_data['name'],
-    user_data['surname'],
-    user_data['patronymic']])
-    connection.commit()
-    if photo_flag:
-        cursor.execute("SELECT max(individual_number) FROM requests;")
-        write_photo(cursor.fetchone(), photo_path)
-    connection.close()
-    await message.answer('Оправлено)')
+    try:
+        conn = connection
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(f"""INSERT INTO requests(item_description, comment_, date_time_of_loss, bus_route, email, phone, name_, fam, surname)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);""",
+        [user_data['lost_thing'],
+        user_data['description'],
+        user_data['date'],
+        user_data['bus'],
+        user_data['email'],
+        user_data['phone'],
+        user_data['name'],
+        user_data['surname'],
+        user_data['patronymic']])
+        connection.commit()
+        if photo_flag:
+            cur.execute("SELECT max(individual_number) FROM requests;")
+            write_photo(cur.fetchone(), photo_path)
+
+    except psycopg2.InterfaceError as exc:
+        print(exc)
+        conn = connection
+        conn.autocommit = True
+        cur = conn.cursor()
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+
+    await callback.message.answer('Оправлено)', reply_markup=ReplyKeyboardRemove())
 
 def register_handlers_fill_form(dp: Dispatcher):
     dp.register_message_handler(insert_form_phone_1, commands=['form'])
@@ -192,6 +221,6 @@ def register_handlers_fill_form(dp: Dispatcher):
     dp.register_message_handler(insert_form_bus_number, state=Form.bus)
     dp.register_message_handler(insert_form_short_desc, state=Form.short_desc)
     dp.register_message_handler(insert_form_description, state=Form.description)
-    dp.register_message_handler(data_db, text = 'ОК', state=Form.send_to_db)
+    dp.register_callback_query_handler(data_db, text = 'Отправить заявку', state=Form.send_to_db)
     dp.register_message_handler(result, content_types=['photo', 'text'], state=Form.result)
-    dp.register_callback_query_handler(cancel_handler, state='*')
+    dp.register_callback_query_handler(cancel_handler, text = 'cancel', state='*')
